c82b85aad508a4ab5a109c166b1ec2afd9923d4e
dgghun <gabriel3052000@yahoo.com>
2019-06-11 12:21:21 -0700

    able to parse post JSON data from pug form

--- controllers/crud.js
+++ controllers/crud.js
@@ -1,14 +1,13 @@
 exports.db_create = function(req, res, next){
     console.log("MESSAGE: db_create() called.");
-    console.log(JSON.stringify(req.body));
+    // console.log(JSON.stringify(req.body));
     
-    var userInfo = JSON.stringify(req.body);
-    console.log(userInfo.firsName);
-    console.log(userInfo.lastName);
-    console.log(userInfo.email);
-    console.log(userInfo.phone);
-    console.log(userInfo.country);
-    console.log(userInfo.password);
+    console.log("---> FirstName:" + req.body.firstName);
+    console.log("---> LastName: " + req.body.lastName);
+    console.log("---> Email:    " + req.body.email);
+    console.log("---> Phone:    " + req.body.phone);
+    console.log("---> Country:  " + req.body.country);
+    console.log("---> Password: " + req.body.password);
 
     res.render('crud_testing');
 }
