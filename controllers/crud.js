1e2a0c6a4be1c9ccaa1e4ec1c9b5d3eb0d6b8371
dgghun <gabriel3052000@yahoo.com>
2019-06-11 12:11:44 -0700

    added JSON body-parser for express

--- controllers/crud.js
+++ controllers/crud.js
@@ -1,5 +1,14 @@
 exports.db_create = function(req, res, next){
-    console.log("stub: db_create() called");
+    console.log("MESSAGE: db_create() called.");
     console.log(JSON.stringify(req.body));
+    
+    var userInfo = JSON.stringify(req.body);
+    console.log(userInfo.firsName);
+    console.log(userInfo.lastName);
+    console.log(userInfo.email);
+    console.log(userInfo.phone);
+    console.log(userInfo.country);
+    console.log(userInfo.password);
+
     res.render('crud_testing');
 }
