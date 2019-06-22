1a07f1b7a1d3c7cb3d830af60b5472e6187bdcf7
VonHohenheim <VonHohenheim@users.noreply.github.com>
2019-06-21 20:16:06 -0700

    Add logout from client page_#13
    - Added mock button to take user back to landing page.
    
    Note: Currently not using sessions or cookies in application. Sould
    probably revist this when we get those put in.
    
    Fixes #13

--- controllers/landing.js
+++ controllers/landing.js
@@ -7,6 +7,13 @@ exports.get_landing = (req, res, next) => res.render("landing", { title: app_nam
 exports.client_signup = (req, res, next) => res.render("signup_client");
 exports.lawyer_signup = (req, res, next) => res.render("signup_lawyer");
 
+exports.logout = (req, res, next) => {
+    //TODO: When sessions or cookies are implemented
+    //  we should destroy any that exist.
+
+    res.render('landing');
+};
+
 exports.crud_testing = function(req, res, next) {
   res.render("crud_testing", {
     title: app_name,
